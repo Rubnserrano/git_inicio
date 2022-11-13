028f13527f1b594cad3718ed9cb1ece53f0bf7c0
Rubnserrano <contactorubenserrano@gmail.com>
2022-11-13 18:39:07 +0100

    primer commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunircn app:app
---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.route('/predict')
+def predict():
+    return 'Predicciones'
+
+if __name__ == '__main__':
+    app.run()
